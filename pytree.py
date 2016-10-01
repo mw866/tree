4fdb16ae51e5552e79529848d9a5851429ef3183
Chris M Wang <mw866@cornell.edu>
2016-10-01 14:52:01 -0400

    test

--- pytree.py
+++ pytree.py
@@ -2,23 +2,27 @@
 import subprocess
 import sys
 import os
-
+dirno=0
+fileno=0
 
 # YOUR CODE GOES here
 def tree(pathname, indention):
+    global dirno, fileno
     dirname, filename = os.path.split(pathname)
     print(indention+'├──', filename)
-    #if a dir
-    if os.path.isdir(pathname):
-            for list_item in os.listdir(pathname):
-                    tree(os.path.join(dirname, list_item),indention+"│  ")
+    if os.path.isdir(pathname):#if dir
+        dirno+=1
+        for list_item in os.listdir(pathname):
+                #print(os.path.join(pathname, list_item))
+                if not list_item.startswith('.'): tree(os.path.join(pathname, list_item),indention+"│  ")
+    else:#if file
+        fileno+=1
 
 if __name__ == '__main__':
-    # just for demo
-    #subprocess.run(['tree'] + sys.argv[1:])
     print('.')
     if len(sys.argv)>= 2:
         for input_path in sys.argv[1:]:
             tree(input_path,'')
     else:
         tree(os.getcwd(),'')
+    print(dirno,' directories, ',fileno,' files')
