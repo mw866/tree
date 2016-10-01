743511b566bf3468977208ff99d77ac010bb00bd
Chris M Wang <mw866@cornell.edu>
2016-10-01 15:50:35 -0400

    test

--- pytree.py
+++ pytree.py
@@ -23,11 +23,22 @@ def tree(pathname, indention):
     else:#if file
         fileno += 1
 
+def init(pathname, indention):
+    ls_items = os.listdir(pathname)
+    for ls_item in ls_items:
+            if not ls_item.startswith('.'):#if non-hidden
+                if ls_item == ls_items[-1]:
+                    new_indention = indention + '└──'
+                else:
+                    new_indention = indention + '├──'
+                tree(os.path.join(pathname, ls_item), new_indention)
+
+
 if __name__ == '__main__':
     print('.')
     if len(sys.argv)>= 2:
         for input_path in sys.argv[1:]:
-            tree(input_path,'')
+            init(input_path,'')
     else:
-        tree(os.getcwd(),'')
+        init(os.getcwd(),'')
     print('\n',dirno,' directories, ',fileno,' files')
