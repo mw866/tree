acdf7e1ffcae684020ce7f31190398d284359d80
mw866 <mw866@cornell.edu>
2016-10-02 13:50:00 -0400

    test

--- pytree.py
+++ pytree.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 import os
 import locale
-dirno = 0
 locale.setlocale(locale.LC_ALL, '')
+dirno = 0
 fileno = 0
 
 
@@ -25,36 +25,31 @@ def tree(pathname, isLast_list):
     print(prefix, filename)
 
     if os.path.isdir(pathname):  # for dir
-      dirno += 1
-      ls_items = [ls_item for ls_item in sorted(os.listdir(pathname), key=locale.strxfrm) if not ls_item.startswith('.')]
-
-      for ls_item in ls_items:
-            if not ls_item.startswith('.'):  # if non-hidden
-                if ls_item == ls_items[-1]:
-                    tree(os.path.join(pathname, ls_item), isLast_list + [True])
-                else:
-                    tree(os.path.join(pathname, ls_item), isLast_list + [False])
+        dirno += 1
+        recurse(pathname, isLast_list)
     else:  # for file
         fileno += 1
 
 
-def root(pathname, isLast_list):
+def recurse(pathname, isLast_list):
     ls_items = [ls_item for ls_item in sorted(os.listdir(pathname), key=locale.strxfrm) if not ls_item.startswith('.')]
 
     for ls_item in ls_items:
-        #if not ls_item.startswith('.'):  # if non-hidden
-            if ls_item == ls_items[-1]:
-                tree(os.path.join(pathname, ls_item), isLast_list + [True])
-            else:
-                tree(os.path.join(pathname, ls_item), isLast_list + [False])
+        if ls_item == ls_items[-1]:
+            tree(os.path.join(pathname, ls_item), isLast_list + [True])
+        else:
+            tree(os.path.join(pathname, ls_item), isLast_list + [False])
+
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
         for input_path in sys.argv[1:]:
             print(input_path)
-            root(input_path, [])
+            recurse(input_path, [])
     else:
         print('.')
-        root(os.getcwd(), [])
+        recurse(os.getcwd(), [])
     print('')
     print(dirno, 'directories,', fileno, 'files')
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
